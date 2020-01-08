918d788ea2226d036538c0f14bf1ff1c1b96312b
Mohamed Zaki <mohamed_zaki888@live.com>
2020-01-08 11:25:59 +0200

    constants file

---
+++ EMBERMedicalTask/EMBERMedicalTask/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  EMBERMedicalTask
+//
+//  Created by Mohamed Mahmoud Zaki on 1/8/20.
+//  Copyright © 2020 Mohamed Mahmoud Zaki. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    static let apiKey = "9b5c6dbf1ea04446a21254b33cd11f5e"
+    static let baseURL = "https://newsapi.org/v2"
+    static let endPoint = "/top-headlines"
+}
