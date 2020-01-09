78c01567beaaa6ff659fc584cc6eecf39de25178
Mohamed Zaki <mohamed_zaki888@live.com>
2020-01-09 10:32:06 +0200

    add country table view

--- EMBERMedicalTask/EMBERMedicalTask/Constants.swift
+++ EMBERMedicalTask/EMBERMedicalTask/Constants.swift
@@ -13,3 +13,7 @@ struct Constants {
     static let baseURL = "https://newsapi.org/v2"
     static let endPoint = "/top-headlines"
 }
+
+struct Countries {
+    static let supportedCountries = ["ae","ar","at", "au" ,"be" ,"bg" ,"br", "ca", "ch" ,"cn", "co", "cu", "cz", "de", "eg", "fr", "gb", "gr", "hk", "hu", "id", "ie", "il", "in", "it", "jp", "kr", "lt", "lv", "ma", "mx", "my", "ng", "nl", "no", "nz", "ph", "pl", "pt", "ro", "rs", "ru", "sa", "se", "sg", "si", "sk", "th", "tr", "tw", "ua" ,"us", "ve", "za"]
+}
--- EMBERMedicalTask/EMBERMedicalTask/Filter popup/FilterPopupViewController.swift
+++ EMBERMedicalTask/EMBERMedicalTask/Filter popup/FilterPopupViewController.swift
@@ -10,13 +10,69 @@ import UIKit
 
 class FilterPopupViewController: UIViewController {
 
+    @IBOutlet weak var selectCountryTableView: UITableView!
+
+    @IBOutlet weak var countryButton: UIButton!
+    @IBOutlet weak var sourceButton: UIButton!
+
+    var countryButtonClicked = false
     var delegate: PopUpDelegate?
     override func viewDidLoad() {
         super.viewDidLoad()
+        countryButton.setTitle("Select Country", for: .normal)
+        sourceButton.titleLabel?.text = "Select News Source"
+
     }
-    
 
     @IBAction func closeButton(_ sender: Any) {
         delegate?.closePopup()
+    }
+
+    @IBAction func sourceButton(_ sender: Any) {
+    }
+
+    fileprivate func showHideCountryTableView() {
+        countryButtonClicked = !countryButtonClicked
+        selectCountryTableView.alpha = countryButtonClicked ? 1 : 0
+        selectCountryTableView.isHidden = !countryButtonClicked
+    }
+
+    @IBAction func selectCountryButton(_ sender: Any) {
+        showHideCountryTableView()
+    }
+}
+
+extension FilterPopupViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch tableView.tag {
+        case 0:
+                return Countries.supportedCountries.count
+        default:
+            return 5
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        switch tableView.tag {
+        case 0:
+            cell.textLabel?.text = Countries.supportedCountries[indexPath.row]
+        default:
+            cell.textLabel?.text = "Done"
+        }
+        return cell
+    }
+
+}
+
+extension FilterPopupViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        switch tableView.tag {
+        case 0:
+            countryButton.setTitle(Countries.supportedCountries[indexPath.row], for: .normal)
+            showHideCountryTableView()
+        default:
+            sourceButton.titleLabel?.text = ""
+        }
     }
 }
